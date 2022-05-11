19615e775d4d37de50f414b1a4fb0944c59afe0c
jenna-graham <jclayto2@gmail.com>
2022-05-11 16:44:43 -0700

    added detail page, added link from main page to detail page

--- render-utils.js
+++ render-utils.js
@@ -8,14 +8,10 @@ export function renderCrystal(crystal) {
     const img = document.createElement('img');
     img.src = crystal.image;
 
-    const p = document.createElement('p');
-    p.textContent = `${crystal.name} is ${crystal.color} and is the stone for the zodiac sign of ${crystal.zodiac}.`;
-
-    const h2 = document.createElement('h2');
-    h2.textContent = `it is the stone of ${crystal.healing} and is connected to the ${crystal.chakra} chakra.`;
-
-    
+    const a = document.createElement('a');
+    a.href = `/details/?id=${crystal.id}`;
      
-    div.append(h1, img, p, h2);
+    a.append(h1, img);
+    div.append(a);
 
     return div;}
