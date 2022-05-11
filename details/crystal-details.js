c486242a6d03677460bc7107dabd87d0fde5af7f
jenna-graham <jclayto2@gmail.com>
2022-05-11 16:52:42 -0700

    got url search params working

--- details/crystal-details.js
+++ details/crystal-details.js
@@ -1,1 +1,14 @@
+import { getCrystalById } from '../fetch-utils.js';
+
+const crystalName = document.getElementById('crystal-name'); 
+
+async function loadData() {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+    const crystal = await getCrystalById(id);
+    crystalName.textContent = crystal.name;
+    
+}
+loadData();
+
 
--- render-utils.js
+++ render-utils.js
@@ -2,8 +2,7 @@ export function renderCrystal(crystal) {
     const div = document.createElement('div');
     div.classList.add('crystal');
 
-    const h1 = document.createElement('h1');
-    h1.textContent = crystal.name;
+    
 
     const img = document.createElement('img');
     img.src = crystal.image;
@@ -11,7 +10,7 @@ export function renderCrystal(crystal) {
     const a = document.createElement('a');
     a.href = `/details/?id=${crystal.id}`;
      
-    a.append(h1, img);
+    a.append(img);
     div.append(a);
 
     return div;}
