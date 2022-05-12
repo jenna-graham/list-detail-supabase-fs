4b9ffe56f08ab1ed126ecc9d81835c5b3f595d6a
jenna-graham <jclayto2@gmail.com>
2022-05-11 18:02:28 -0700

    added get item by id supabase function to fetch utils. added load data function

--- details/crystal-details.js
+++ details/crystal-details.js
@@ -1,12 +1,15 @@
 import { getCrystalById } from '../fetch-utils.js';
+import { renderCrystals } from '../render-utils.js';
 
-const crystalName = document.getElementById('crystal-name'); 
+const crystals = document.getElementById('crystals'); 
 
 async function loadData() {
     const params = new URLSearchParams(window.location.search);
-    const id = params.get('id');
-    const crystal = await getCrystalById(id);
-    crystalName.textContent = crystal.name;
+    const crystalId = params.get('id');
+    const crystal = await getCrystalById(crystalId);
+
+    const crystalDiv = renderCrystals(crystal);
+    crystals.append(crystalDiv);
     
 }
 loadData();
